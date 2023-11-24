95858d406b10ad74b70b2badd9e9b65319f34d8f
Mathis Verleene <mathis.verleene1@decathlon.com>
2023-11-24 15:20:13 +0100

    Add tests

--- tests/users.test.js
+++ tests/users.test.js
@@ -24,3 +24,19 @@ describe('POST /users/add', () => {
         expect(response.body.details).toBe('Invalid email or username format');
     });
 });
+
+describe('POST /users/add', () => {
+    it("should post a new user and get it on /users", async () => {
+        const response = await supertest(app)
+            .post('/users/add')
+            .send({ username: 'John', email: 'johndoe@yopmail.com'})
+        expect(response.status).toBe(200)
+        expect(response.body.message).toBe('User added successfully')
+        const users = await supertest(app)
+            .get('/users')
+
+        expect(users.body.length).toBe(1)
+        expect(users.body[0].username).toBe('John')
+        expect(users.body[0].email).toBe('johndoe@yopmail.com')
+    })
+})
